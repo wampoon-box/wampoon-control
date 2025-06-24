ba97b51e8ec54b4306b15e3a1a5e1bda94918940
frostybee <rsleiman2@gmail.com>
2025-06-24 13:44:02 -0400

    Clean up unwanted methods

--- src/Pwamp.ControlPanel/Source/Controllers/ServerPathManager.cs
+++ src/Pwamp.ControlPanel/Source/Controllers/ServerPathManager.cs
@@ -23,30 +23,18 @@ namespace Frostybee.Pwamp.Controllers
             _diagnostics = new ServerDiagnostics(fileOps, _pathResolver);
         }
 
-        public static string ApplicationDirectory => _pathResolver.ApplicationDirectory;
         public static string AppsDirectory => _pathResolver.AppsDirectory;
         public static string ApacheDocumentRoot => _pathResolver.ApacheDocumentRoot;
 
         public static ServerPathInfo GetServerPath(string serverName) => _pathResolver.GetServerPath(serverName);
-        public static Dictionary<string, ServerPathInfo> GetAllServerPaths() => _pathResolver.GetAllServerPaths();
-        public static List<ServerPathInfo> GetAvailableServers() => _pathResolver.GetAvailableServers();
         public static string GetExecutablePath(string serverName) => _pathResolver.GetExecutablePath(serverName);
         public static string GetConfigPath(string serverName) => _pathResolver.GetConfigPath(serverName);
         public static string GetSpecialPath(string serverName, string pathType) => _pathResolver.GetSpecialPath(serverName, pathType);
         public static bool IsServerAvailable(string serverName) => _pathResolver.IsServerAvailable(serverName);
-        public static string GetServerBinDirectory(string serverName) => _pathResolver.GetServerBinDirectory(serverName);
         public static string GetServerBaseDirectory(string serverName) => _pathResolver.GetServerBaseDirectory(serverName);
         public static List<string> GetServerNames() => _pathResolver.GetServerNames();
-        public static List<string> GetAvailableServerNames() => _pathResolver.GetAvailableServerNames();
-        public static void RefreshServerAvailability() => _pathResolver.RefreshServerAvailability();
 
-        public static bool OpenConfigFile(string serverName) => _fileOperations.OpenConfigFile(serverName);
-        public static bool OpenFileInDefaultEditor(string filePath) => _fileOperations.OpenFileInDefaultEditor(filePath);
-        public static bool CanOpenConfigFile(string serverName) => _fileOperations.CanOpenConfigFile(serverName);
-        public static string GetConfigFileSize(string serverName) => _fileOperations.GetConfigFileSize(serverName);
-        public static DateTime? GetConfigFileLastModified(string serverName) => _fileOperations.GetConfigFileLastModified(serverName);
 
-        public static Dictionary<string, string> GetDiagnosticInfo() => _diagnostics.GetDiagnosticInfo();
         public static void LogApacheDiagnostics() => _diagnostics.LogApacheDiagnostics();
     }    
 }
