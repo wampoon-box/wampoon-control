643730377c616d02e3492a471a42147564c87f01
frostybee <rsleiman2@gmail.com>
2025-07-10 15:33:37 -0400

    Renam the app

--- src/Wampoon.ControlPanel/Source/Helpers/AppConstants.cs
+++ src/Wampoon.ControlPanel/Source/Helpers/AppConstants.cs
@@ -60,7 +60,7 @@ namespace Wampoon.ControlPanel.Helpers
             public const string APACHE_BIN = "bin";
             public const string APACHE_LOGS = "logs";
             public const string MYSQL_DATA = "data";
-            public const string CUSTOM_CONFIG_NAME = "pwamp-custom-path.conf";
+            public const string CUSTOM_CONFIG_NAME = "wampoon-custom-path.conf";
         }
         
         // UI Constants
